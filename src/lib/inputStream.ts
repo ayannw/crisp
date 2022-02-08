289689ba9884b6700b46f5b5bf87789238608adf
ayannw <ayanispro420@gmail.com>
2022-02-08 10:57:51 +0600

    create lib/inputStream.ts

---
+++ src/lib/inputStream.ts
@@ -0,0 +1,36 @@
+class InputStream {
+  public pos: number;
+  public line: number;
+  public col: number;
+
+  constructor(private input: string) {
+    this.pos = 0;
+    this.line = 1;
+    this.col = 0;
+  }
+
+  public next(): string {
+    let ch = this.input.charAt(this.pos++);
+    if (ch == "\n") this.line++, (this.col = 0);
+    else this.col++;
+    return ch;
+  }
+
+  public peek(): string {
+    return this.input.charAt(this.pos);
+  }
+
+  public eof(): boolean {
+    return this.peek() == "";
+  }
+
+  public error(msg: string): Error {
+    throw new Error(msg + " (" + this.line + ":" + this.col + ")");
+  }
+}
+
+const inputStream = (input: string) => {
+  return new InputStream(input);
+};
+
+export default inputStream;
